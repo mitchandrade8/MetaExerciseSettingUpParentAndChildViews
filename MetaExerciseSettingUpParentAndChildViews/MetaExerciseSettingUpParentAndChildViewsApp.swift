03acc45fd23a7153187dd49c6b8a037481cb36ec
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-04 10:22:43 -0600

    Initial Commit

---
+++ MetaExerciseSettingUpParentAndChildViews/MetaExerciseSettingUpParentAndChildViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaExerciseSettingUpParentAndChildViewsApp.swift
+//  MetaExerciseSettingUpParentAndChildViews
+//
+//  Created by Mitch Andrade on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaExerciseSettingUpParentAndChildViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
